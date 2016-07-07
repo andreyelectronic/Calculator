9989bb9f9b0fa15198d52b5857c30d27c5758917
andreyelectronic <noreply>
2016-07-07 11:22:56 +0000

    Add component logo

---
+++ ui/logo.reel/logo.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/logo.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Logo
+ * @extends Component
+ */
+exports.Logo = Component.specialize(/** @lends Logo# */ {
+    constructor: {
+        value: function Logo() {
+            this.super();
+        }
+    }
+});
